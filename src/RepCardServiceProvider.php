500b478281972eccc6274719f7764c58088a98b8
Kyle Anderson <kanderson@eliosfund.com>
2024-07-15 20:34:03 -0500

    Extract to construct

--- src/RepCardServiceProvider.php
+++ src/RepCardServiceProvider.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace RepCard;
 
-use GuzzleHttp\Psr7\Uri;
 use Illuminate\Contracts\Support\DeferrableProvider;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class RepCardServiceProvider extends ServiceProvider implements DeferrableProvider
@@ -24,25 +22,10 @@ class RepCardServiceProvider extends ServiceProvider implements DeferrableProvid
             $this->mergeConfigFrom($configPath, 'repcard');
         }
 
-        $this->app->singleton(RepCardService::class, static fn (): RepCardService => new RepCardService(
-            client: Http::acceptJson()->baseUrl(
-                url: (string) (new Uri())->withScheme(
-                    scheme: 'https'
-                )->withHost(
-                    host: config('repcard.fqdn')
-                )->withPath(
-                    path: config('repcard.endpoint')
-                )
-            )->withHeader(
-                name: 'x-api-key',
-                value: config('repcard.key')
-            )->timeout(
-                seconds: config('repcard.timeout')
-            )->connectTimeout(
-                seconds: config('repcard.connect_timeout')
-            ),
-            companyId: config('repcard.company_id')
-        ));
+        $this->app->singleton(
+            abstract: RepCardService::class,
+            concrete: static fn (): RepCardService => new RepCardService()
+        );
     }
 
     /**
