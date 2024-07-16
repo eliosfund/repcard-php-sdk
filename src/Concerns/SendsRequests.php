cef7364ab8d986de8ca1e8c96d445382e06aaccd
Kyle Anderson <kanderson@eliosfund.com>
2024-07-15 20:36:19 -0500

    Add method to get client

--- src/Concerns/SendsRequests.php
+++ src/Concerns/SendsRequests.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace RepCard\Concerns;
 
+use GuzzleHttp\Psr7\Uri;
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use RepCard\RepCardService;
 
 /**
@@ -21,12 +25,37 @@ trait SendsRequests
         $this->companyId = config('repcard.company_id');
     }
 
+    public function baseUri(): Uri
+    {
+        return (new Uri())->withScheme(
+            scheme: 'https'
+        )->withHost(
+            host: config('repcard.fqdn')
+        )->withPath(
+            path: config('repcard.endpoint')
+        );
+    }
+
+    public function client(): PendingRequest
+    {
+        return Http::acceptJson()->baseUrl(
+            url: (string) $this->baseUri()
+        )->withHeader(
+            name: 'x-api-key',
+            value: config('repcard.key')
+        )->timeout(
+            seconds: config('repcard.timeout')
+        )->connectTimeout(
+            seconds: config('repcard.connect_timeout')
+        );
+    }
+
     /**
      * Issue a `GET` request to the given path.
      */
     public function get(string $path, ?array $query = null): Response
     {
-        return $this->client->get($path, $query ?? []);
+        return $this->client()->get($path, $query);
     }
 
     /**
@@ -34,7 +63,7 @@ trait SendsRequests
      */
     public function post(string $path, array $data = []): Response
     {
-        return $this->client->post($path, $data);
+        return $this->client()->post($path, $data);
     }
 
     /**
@@ -42,6 +71,6 @@ trait SendsRequests
      */
     public function put(string $path, array $data = []): Response
     {
-        return $this->client->put($path, $data);
+        return $this->client()->put($path, $data);
     }
 }
